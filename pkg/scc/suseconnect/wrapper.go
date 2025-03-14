4660a59dd054ab8d114caa7beb169eb20a30de8f
Dan Pock <self@danpock.me>
2025-03-13 21:00:48 -0400

    set language to en_US by default

--- pkg/scc/suseconnect/wrapper.go
+++ pkg/scc/suseconnect/wrapper.go
@@ -6,7 +6,7 @@ func DefaultConnectionOptions() connection.Options {
 	// TODO: I believe this is creating the options for the API "app"
 	// So this doesn't necessarily mean these have to match Rancher on the cluster.
 	// Rather the details about the HTTP client talking to SCC
-	return connection.DefaultOptions("rancher-scc-integration", "v0.0.1", "todo lang")
+	return connection.DefaultOptions("rancher-scc-integration", "0.0.1", "en_US")
 }
 
 func DefaultRancherConnection() *connection.ApiConnection {
