b48965986904523649a50d7ce693b381680685ef
Dan Pock <self@danpock.me>
2025-03-13 22:08:07 -0400

    Add outline for overall concept of how to handle this

--- pkg/scc/operator.go
+++ pkg/scc/operator.go
@@ -10,8 +10,14 @@ func Register(
 	ctx context.Context,
 	wContext *wrangler.Context,
 ) error {
+	// TODO: Track if this cluster has had registration operator started ever
+	// TODO: On first boot, check/fetch the ConfigMap/Secrets to populate a RegistrationRequest
+	// This should be skipped on subsequent starts of the cluster
+
 	// TODO register controllers here
 	logrus.Info("Register controllers here")
+
+	// TODO: Some where in operator, or in registration controller, the current Registration needs to be revalidated every 24 hours
 
 	return nil
 }
