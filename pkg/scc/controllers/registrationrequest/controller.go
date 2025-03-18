5dce8f86c0ea4f77d81ed32fc42c994d10b9e3a4
Dan Pock <self@danpock.me>
2025-03-17 20:45:39 -0400

    Refactor online vs offline requests

--- pkg/scc/controllers/registrationrequest/controller.go
+++ pkg/scc/controllers/registrationrequest/controller.go
@@ -2,6 +2,11 @@ package registrationrequest
 
 import (
 	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/rancher/rancher/pkg/scc"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/kubernetes/pkg/apis/core"
 
 	v1 "github.com/rancher/rancher/pkg/apis/scc.cattle.io/v1"
 	registrationControllers "github.com/rancher/rancher/pkg/generated/controllers/scc.cattle.io/v1"
@@ -33,15 +38,64 @@ func Register(
 	registrationRequests.OnChange(ctx, "registrationRequests", controller.OnRegistrationRequestChange)
 }
 
-func (h *handler) OnRegistrationRequestChange(key string, registrationRequest *v1.RegistrationRequest) (*v1.RegistrationRequest, error) {
-	logrus.Infof("[scc.registrationrequest-controller]: Received registrationRequest %q", key)
+func (h *handler) OnRegistrationRequestChange(name string, registrationRequest *v1.RegistrationRequest) (*v1.RegistrationRequest, error) {
+	if registrationRequest == nil {
+		return nil, nil
+	}
+
+	logrus.Infof("[scc.registrationrequest-controller]: Received registrationRequest %q", name)
 	logrus.Info("[scc.registrationrequest-controller]: RegistrationRequest ", registrationRequest)
 	// TODO: handle the logic of what happens when one of these is created
 	// Gist being:
 	// 1. Verify RegistrationRequest is not already fulfilled or expired,
+	if registrationRequest.Status.RequestProcessedTS != "" {
+		logrus.Info("[scc.registrationrequest-controller]: RegistrationRequest already processed")
+		return registrationRequest, nil
+	}
 	// 2. Verify contents of RegistrationRequest (mode and creds),
-	// 3. Attempt SCC phone home with Online mode OR process the offline mode
+	if registrationRequest.Spec.Mode == v1.Online {
+		err := h.processOnlineRegistration(registrationRequest)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		err := h.processOfflineRegistration(registrationRequest)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	// 4. At the end of either process the current RegistrationRequest is either: a) fulfilled, b) expired or c) failed (retry?)
 	// 4+. If it was a success, then a new Registration is created (and the old one deleted or marked as not current?)
 	return registrationRequest, nil
+}
+
+func (h *handler) processOnlineRegistration(registrationRequest *v1.RegistrationRequest) error {
+	logrus.Info("[scc.registrationrequest-controller]: online mode ")
+	// 1. Verify the secret ref name
+	secretName := scc.RegCodeSecretName
+	if registrationRequest.Spec.RegistrationCodeSecretRef != nil {
+		secretName = registrationRequest.Spec.RegistrationCodeSecretRef.Name
+	}
+
+	// 2. Verify RegistrationRequest creds,
+	regSecret, err := h.secrets.Get("", secretName, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	_, ok := regSecret.Data[scc.RegCodeSecretKey]
+	if !ok {
+		return errors.New(fmt.Sprintf("registration secret `%s` does not contain expected data `%s`", secretName, scc.RegCodeSecretKey))
+	}
+
+	// 2. Attempt SCC phone home with Online mode OR process the offline mode
+
+	return nil
+}
+
+func (h *handler) processOfflineRegistration(registrationRequest *v1.RegistrationRequest) error {
+	// TODO implement offline mechanism
+	logrus.Info("[scc.registrationrequest-controller]: offline mode ")
+	return nil
 }
