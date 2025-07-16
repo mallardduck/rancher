4a2f451c030b7d703b5952fa0975d3846053925b
Dan Pock <self@danpock.me>
2025-07-16 17:23:45 -0400

    Update shell to un-RC version

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -4,7 +4,7 @@ package buildconfig
 
 const (
 	CspAdapterMinVersion     = "107.0.0+up7.0.0-rc.1"
-	DefaultShellVersion      = "rancher/shell:v0.5.0-rc.4"
+	DefaultShellVersion      = "rancher/shell:v0.5.0"
 	FleetVersion             = "107.0.0+up0.13.0-beta.4"
 	ProvisioningCAPIVersion  = "107.0.0+up0.8.0"
 	RemoteDialerProxyVersion = "106.0.0+up0.4.4"
